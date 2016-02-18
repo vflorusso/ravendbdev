ee644fa512359aab63925b672561bd21edeb5f81
Vito Flavio Lorusso <vf.lorusso@live.com>
2016-02-18 08:15:22 +0100

    commit first version

---
+++ deploy-template.ps1
@@ -0,0 +1,11 @@
+﻿Login-AzureRmAccount$name = "vitoraventest01"
+$resourcegroupName = $name
+
+Select-AzureRmSubscription -SubscriptionName "vitolo-internal"
+
+New-AzureRmResourceGroup -Name $resourcegroupName -Location "West Europe"
+
+New-AzureRmResourceGroupDeployment -Name $name `
+            -ResourceGroupName $resourcegroupName `
+            -TemplateFile "azuredeploy.json" `
+            -TemplateParameterFile "azuredeploy.parameters.json"
